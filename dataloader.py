c89535bee19e5bc56b61134ba7c9c3253520476c
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-07-12 21:20:48 +0300

    dataloader + tests

--- dataloader.py
+++ dataloader.py
@@ -5,6 +5,121 @@ from random import shuffle
 from itertools import groupby
 
 # third-party imports
-import tensorflow as tf
-import youtokentome
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf  # type: ignore
+import youtokentome  # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences  # type: ignore
+
+
+class SequenceLoader(object):
+    """
+    An iterator for loading batches of data into the transformer model.
+
+    For training:
+
+        Each batch contains tokens_in_batch target language tokens (approximately),
+        target language sequences of the same length to minimize padding and therefore memory usage,
+        source language sequences of very similar lengths to minimize padding and therefore memory usage.
+        Batches are also shuffled.
+
+    For validation and testing:
+
+        Each batch contains a single source-target pair, in the same order as in the files from which they were read.
+    """
+
+    def __init__(self, data_folder, source_suffix, target_suffix, split, tokens_in_batch):
+        """
+        Sequence constructor.
+
+        :param data_folder: folder containing the source and target language data files
+        :param source_suffix: the filename suffix for the source language
+        :param target_suffix: the filename suffix for the target language
+        :param split: train, val or test
+        :param tokens_in_batch: the number of target language tokens in each batch
+        """
+        self.n_batches = None
+        self.all_batches = None
+        self.current_batch = None
+
+        self.tokens_in_batch = tokens_in_batch
+        self.source_suffix = source_suffix
+        self.target_suffix = target_suffix
+
+        assert split.lower() in {"train", "test", "val"}, "no such split"
+
+        self.split = split.lower()
+        self.for_training = self.split == "train"
+        self.bpe_model = youtokentome.BPE(model=os.path.join(data_folder, "bpe.model"))
+
+        with codecs.open(os.path.join(data_folder, ".".join([split, source_suffix])), "r", encoding="utf-8") as f:
+            source_data = f.read().split("\n")
+        with codecs.open(os.path.join(data_folder, ".".join([split, target_suffix])), "r", encoding="utf-8") as f:
+            target_data = f.read().split("\n")
+        assert len(source_data) == len(target_data), "different number of source and target sequences"
+
+        source_lengths = [len(s) for s in self.bpe_model.encode(source_data, bos=False, eos=False)]
+        target_lengths = [len(t) for t in self.bpe_model.encode(target_data, bos=True, eos=True)]
+
+        self.data = list(zip(source_data, target_data, source_lengths, target_lengths))
+
+        if self.for_training:
+            self.data.sort(key=lambda x: x[3])
+
+        self.create_batches()
+
+    def create_batches(self):
+        """
+        Prepares batches for one epoch.
+        """
+        if self.for_training:
+            chunks = [list(g) for _, g in groupby(self.data, key=lambda x: x[3])]
+
+            self.all_batches = list()  # create batches with the same target sequence length
+            for chunk in chunks:
+                chunk.sort(key=lambda x: x[2])  # sort so that a batch would also have similar source sequence lengths
+                # div the expected batch size (tokens) by sequence length in this chunk to get # of sequences per batch
+                seqs_per_batch = self.tokens_in_batch // chunk[0][3]
+                self.all_batches.extend([chunk[i: i + seqs_per_batch] for i in range(0, len(chunk), seqs_per_batch)])
+
+            shuffle(self.all_batches)  # shuffle batches
+            self.n_batches = len(self.all_batches)
+            self.current_batch = -1
+
+        else:
+            self.all_batches = [[d] for d in self.data]
+            self.n_batches = len(self.all_batches)
+            self.current_batch = -1
+
+    def __iter__(self):
+        """ Required by iterator."""
+        return self
+
+    @tf.function
+    def __next__(self):
+        """
+        Next in iterator.
+
+        :returns: the next batch, containing:
+            source language sequences, a tensor of size (N, encoder_sequence_pad_length)
+            target language sequences, a tensor of size (N, decoder_sequence_pad_length)
+            true source language lengths, a tensor of size (N)
+            true target language lengths, a tensor of size (N)
+        """
+        self.current_batch += 1
+        try:
+            source_data, target_data, source_lengths, target_lengths = zip(*self.all_batches[self.current_batch])
+        except IndexError:
+            raise StopIteration
+
+        source_data = self.bpe_model.encode(source_data, output_type=youtokentome.OutputType.ID, bos=False, eos=False)
+        target_data = self.bpe_model.encode(target_data, output_type=youtokentome.OutputType.ID, bos=True, eos=True)
+
+        source_data = pad_sequences(sequences=source_data, padding='post', value=self.bpe_model.subword_to_id('<PAD>'))
+        target_data = pad_sequences(sequences=target_data, padding='post', value=self.bpe_model.subword_to_id('<PAD>'))
+
+        source_data = tf.convert_to_tensor(source_data, dtype=tf.int32)
+        target_data = tf.convert_to_tensor(target_data, dtype=tf.int32)
+
+        source_lengths = tf.convert_to_tensor(source_lengths, dtype=tf.int32)
+        target_lengths = tf.convert_to_tensor(target_lengths, dtype=tf.int32)
+
+        return source_data, target_data, source_lengths, target_lengths
---
+++ test/test_dataloader.py
@@ -0,0 +1,72 @@
+# standard imports
+import os
+
+# third-party imports
+import pytest
+import youtokentome
+import tensorflow as tf
+from unittest.mock import patch
+
+# module imports
+from dataloader import SequenceLoader
+
+
+@pytest.fixture(name="train_config")
+def training_configuration():
+    """Configuration parameters for data loader initialization."""
+    config = {
+        "data_folder": "data",
+        "source_suffix": "en",
+        "target_suffix": "de",
+        "split": "train",
+        "tokens_in_batch": 512
+    }
+    return config
+
+
+@pytest.fixture(name="dummy_loader")
+def dummy_sequence_loader(train_config):
+    """Dummy sequence loader with skipped batch generation."""
+    with patch.object(SequenceLoader, 'create_batches', return_value=None) as mock_method:
+        loader = SequenceLoader(data_folder=train_config["data_folder"], source_suffix=train_config["source_suffix"],
+                                target_suffix=train_config["target_suffix"], split=train_config["split"],
+                                tokens_in_batch=train_config["tokens_in_batch"])
+        mock_method.assert_called_once()
+    return loader
+
+
+@pytest.fixture(name="loader")
+def sequence_loader(train_config):
+    """Sequence loader in train configuration."""
+    loader = SequenceLoader(data_folder=train_config["data_folder"], source_suffix=train_config["source_suffix"],
+                            target_suffix=train_config["target_suffix"], split=train_config["split"],
+                            tokens_in_batch=train_config["tokens_in_batch"])
+    return loader
+
+
+def test_sequence_loader_initialization(dummy_loader):
+    """Test initialization of a dummy data loader."""
+    assert isinstance(dummy_loader, SequenceLoader), "mock testing has probably failed"
+
+
+def test_create_batches(loader):
+    """Initialize without mocking `create_batches()`."""
+    assert loader.n_batches == len(loader.all_batches)
+    assert loader.current_batch == -1
+
+
+def test_next_dunder(loader):
+    """Test iteration over data loader"""
+    source_data, target_data, source_lengths, target_lengths = loader.__next__()
+    assert isinstance(source_data, tf.Tensor), "source_data is not a tf.Tensor."
+    assert isinstance(target_data, tf.Tensor), "target_data is not a tf.Tensor."
+    assert isinstance(source_lengths, tf.Tensor), "source_lengths is not a tf.Tensor."
+    assert isinstance(target_lengths, tf.Tensor), "target_lengths is not a tf.Tensor."
+
+    assert tf.rank(source_data) == 2, "source_data should be a 2D Tensor."
+    assert tf.rank(target_data) == 2, "target_data should be a 2D Tensor."
+    assert tf.rank(source_lengths) == 1, "source_lengths should be a 1D Tensor."
+    assert tf.rank(target_lengths) == 1, "target_lengths should be a 1D Tensor."
+
+    assert source_data.shape[0] == target_data.shape[0], "Mismatch between number of source and target sequences."
+    assert source_lengths.shape[0] == target_lengths.shape[0], "Mismatch between number of source and target lengths."
