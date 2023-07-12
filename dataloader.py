6e8fa6d5c168b7f4fa1a66df74eca0229ca05ed9
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-07-12 14:45:20 +0300

    fucking tokenizers

---
+++ dataloader.py
@@ -0,0 +1,10 @@
+# standard imports
+import os
+import codecs
+from random import shuffle
+from itertools import groupby
+
+# third-party imports
+import tensorflow as tf
+import youtokentome
+from tensorflow.keras.preprocessing.sequence import pad_sequences
