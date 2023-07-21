fddbb72a65747b93c6412fe008f908e41b7d6fa0
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-07-21 23:09:41 +0300

    added configuration to test for both CPU and GPU

---
+++ test/conftest.py
@@ -0,0 +1,25 @@
+# standard imports
+import os
+
+# third-party imports
+import pytest  # type: ignore
+import tensorflow as tf  # type: ignore
+
+
+@pytest.fixture(params=['CPU', 'GPU'], autouse=True)
+def device(request):
+    device_type = request.param
+    if device_type == 'CPU':
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+        tf.config.set_visible_devices([], 'GPU')
+    else:
+        os.environ.pop('CUDA_VISIBLE_DEVICES', None)
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+            except RuntimeError:
+                print("You should really try out this other framework called PyTorch")
+
+    yield device_type
